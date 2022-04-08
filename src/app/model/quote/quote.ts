a89e78cefa326412c3eee9db944d0464982d06e2
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-08 23:45:29 +0300

    Add custom model class for the quote

---
+++ src/app/model/quote/quote.ts
@@ -0,0 +1,15 @@
+export class Quote {
+  upvote: number;
+  downvote: number;
+  dateCreated: Date;
+
+  constructor(
+    public id: number,
+    public text: string,
+    public author: string,
+    public submittedBy: string){
+      this.upvote = 0;
+      this.downvote = 0;
+      this.dateCreated = new Date();
+    }
+}
