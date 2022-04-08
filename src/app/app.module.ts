174733fc3e62eb386a2d1863d0f6ea42459a8327
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-08 23:59:55 +0300

    Add component to display form for collection of quote data:quote text, author and person submitting quote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './component/quotes/quotes.component';
 import { QuoteDetailsComponent } from './component/quote-details/quote-details.component';
+import { QuoteFormComponent } from './component/quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    QuoteDetailsComponent
+    QuoteDetailsComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
