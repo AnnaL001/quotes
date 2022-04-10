98f5f36fbe2623edf68b11d24ce576b1fa506253
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-10 14:22:35 +0300

    Add quote input property binding to display a quote and its details

--- src/app/component/quote-details/quote-details.component.ts
+++ src/app/component/quote-details/quote-details.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, Input, OnInit } from '@angular/core';
+import { Quote } from 'src/app/model/quote/quote';
 
 @Component({
   selector: 'app-quote-details',
@@ -6,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+  // Type assertions
+  @Input() quote = {} as Quote;
 
   constructor() { }
 
