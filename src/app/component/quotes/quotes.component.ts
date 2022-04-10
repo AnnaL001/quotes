a9d3086af0e24c5513c262bc3353050e43eb2924
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-10 22:55:38 +0300

    Add functionality to toggle form, submit form details, increase upvotes and increase downvotes

--- src/app/component/quotes/quotes.component.ts
+++ src/app/component/quotes/quotes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Quote } from 'src/app/model/quote/quote';
 import { QuoteService } from 'src/app/service/quote.service';
 
@@ -13,9 +13,46 @@ export class QuotesComponent implements OnInit {
 
   constructor(private quoteService: QuoteService) { }
 
-  ngOnInit(): void {
-    this.quotes = this.quoteService.getQuotes();
-    console.log(this.quotes);
+  displayForm(display: boolean){
+    if(display){
+      let form = document.getElementById('form');
+      form?.classList.remove('hide');
+    }
   }
 
+  toggleQuoteDetails(index: number){
+    this.quotes[index].showDetails = !this.quotes[index].showDetails;
+  }
+
+  ngOnInit(): void {
+    this.quotes = this.quoteService.getQuotes();
+  }
+
+  increaseUpvote(upvote: boolean, index: number){
+    if(upvote){
+      this.quotes[index].upvote++;
+    }
+  }
+
+  increaseDownvote(downvote: boolean, index: number){
+    if(downvote){
+      this.quotes[index].downvote++;
+    }
+  }
+
+  hideForm(isHidden: boolean){
+    if(isHidden){
+      let form = document.getElementById("form");
+      form?.classList.add('hide');
+    }
+  }
+
+  addNewQuote(quote: Quote){
+    quote.id = this.quotes.length + 1;
+    quote.upvote = 0;
+    quote.downvote = 0;
+    quote.dateCreated = new Date();
+    this.quotes.unshift(quote);
+    console.log(this.quotes);
+  }
 }
