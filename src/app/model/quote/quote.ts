158095deabedc6a7fac6a3c8ffd16a2899370d05
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-11 09:23:08 +0300

    Update quote model class

--- src/app/model/quote/quote.ts
+++ src/app/model/quote/quote.ts
@@ -1,17 +1,16 @@
 export class Quote {
   upvote: number;
   downvote: number;
-  dateCreated: Date;
   showDetails: boolean;
 
   constructor(
     public id: number,
     public quoteText: string,
     public author: string,
-    public submittedBy: string){
+    public submittedBy: string,
+    public dateCreated: Date){
       this.upvote = 0;
       this.downvote = 0;
-      this.dateCreated = new Date();
       this.showDetails = false;
     }
 }
