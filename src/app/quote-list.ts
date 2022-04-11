c7f7dcf2c2ee969c4b3d227f621873c19cd32677
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-11 09:23:57 +0300

    Update initial list of quotes

--- src/app/quote-list.ts
+++ src/app/quote-list.ts
@@ -2,35 +2,40 @@ import { Quote } from "./model/quote/quote";
 
 // Initial array with 6 quotes
 export const quotes: Quote[] = [
-  new Quote(1, 'Stay hungry.Stay foolish', 'Steve Jobs', 'Lynn Nyangon'),
+  new Quote(1, 'Stay hungry.Stay foolish', 'Steve Jobs', 'Lynn Nyangon', new Date(2022, 2, 10)),
   new Quote(
     2,
     'Everybody is a genius. But if you judge a fish by its ability to climb a tree, it will live its whole life believing that it is stupid',
     'Albert Einstein',
-    'Lynn Nyangon'
+    'Lynn Nyangon',
+    new Date(2022, 0, 1)
   ),
   new Quote(
     3,
     'The only limit to the height of your achievements is the reach of your dreams and your willingness to work hard for them',
     'Michelle Obama',
-    'Lynn Nyangon'
+    'Lynn Nyangon',
+    new Date(2020, 1, 12)
   ),
   new Quote(
     4,
     "Your time is limited so don't waste it living somebody else's life",
     'Steve Jobs',
-    'Lynn Nyangon'
+    'Lynn Nyangon',
+    new Date(2022, 0, 18)
   ),
   new Quote(
     5,
     'When they go low, we go high',
     'Michelle Obama',
-    'Lynn Nyangon'
+    'Lynn Nyangon',
+    new Date(2011, 3, 6)
   ),
   new Quote(
     6,
     'A person who never made a mistake never tried anything new',
     'Albert Einstein',
-    'Lynn Nyangon'
+    'Lynn Nyangon',
+    new Date(2022, 2, 28)
   )
 ];
